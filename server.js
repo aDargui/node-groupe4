c9e308c0a821d4fab1561060178810d3b68ba79f
leititiab <leititiab@gmail.com>
2021-07-09 12:37:13 +0200

    modification code leititia terminé

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
+const {
+	default: fastifyMongodb
+} = require('fastify-mongodb')
+
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
-const { ObjectId } = require('mongodb')
+const fastify = require('fastify')({
+	logger: true
+})
+const {
+	ObjectId
+} = require('mongodb')
+const argon2 = require('argon2')
+const createError = require('http-errors')
 
-// const jwt = require('jsonwebtoken'); //https://www.npmjs.com/package/jsonwebtoken
-//pour fastify-jwt faut installer : npm install fastify-jwt
-// https://www.npmjs.com/package/fastify-jwt
 fastify.register(require('fastify-jwt'), {
-  secret: 'supersecret'
+	secret: 'monsupersecretestmoi'
+	// le secret ne doit être divulgué à personne 
 })
-
-// Faut installer npm i fastify-cors
-fastify.register(require ('fastify-cors'),{
-  origin: "*"
-})
-
-// Connexion à la base de donnee -- https://github.com/fastify/fastify-mongodb
+// Connexion à la BDD
 fastify.register(require('./connector'))
-
-// Importation de la route heroes
+//importation de la route "heroes"
 fastify.register(require('./src/routes/heroes'))
-
-// Importation de la route users
+//importation de la route "users"
 fastify.register(require('./src/routes/users'))
 
-// Importation de la route / login
-fastify.register(require('./src/routes/login'))
 
 
 // METHOD API REST
@@ -33,39 +31,33 @@ fastify.register(require('./src/routes/login'))
 // PATCH / PUT - UPDATE
 // DELETE - DELETE
 
-
 // Declare a route
 fastify.get('/', (request, reply) => {
-  return { hello: 'world 2' }
-})
-
-// Déclarer la route /heroes - cette route retournera la liste des avengers
-const avengers = ["Iron man", "Captain america", "Spiderman"]
-
-// heroes GET - Obtiens la liste des héros
-fastify.get('/heroesTest', ()=>{
-    return avengers //equivalent à avengers: avengers
+	// Ici on retourne un objet javascript qui va être converti en JSON (JavaScript Object Notation)
+	return {
+		hello: 'world'
+	}
 })
 
 
-fastify.get('/me', (request, reply) => {
-    //ici on retourne un objet javascript qui va etre converti en json
-    //(JavaScript Object Notation)
-    return { 
-        prenom: 'Abdallah',
-        nom: "Dargui d",
-        job: "developpeur web 3",
-     }
-  })
+
+fastify.get('/me', function () {
+	return {
+		prenom: "Leititia",
+		nom: "BOUANGA",
+		job: "chef de projet web",
+	}
+})
+
 
 // Run the server!
 const start = async () => {
-  try {
-    await fastify.listen(3000)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+	try {
+		console.log("Serveur lancé: http:// localhost:4000")
+		await fastify.listen(4000)
+	} catch (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
 }
 start()
-
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,55 +1,60 @@
-const argon2 = require('argon2') //https://www.npmjs.com/package/argon2
-const createError = require('http-errors') //https://www.npmjs.com/package/http-errors
+const { ObjectId } = require('mongodb') 
+const argon2 = require('argon2')
 
-async function routes (fastify, options) {
-// Faut d'abord installer : npm install jsonwebtoken
-  // https://www.npmjs.com/package/jsonwebtoken
-  
-  fastify.post('/login', async (request,replay) =>{
-    //Je récupère l'email et le password dans request,
-    // Je cherche si un utiliseur possede cet email,
-    // s'il existe, on vérifie que les password correspondent
-    // Sinon, on génère une erreur
-  
-    const {email, password} = request.body
-    const collection = fastify.mongo.db.collection('users')
-    const userExists = await collection.findOne({email})
-  
-    if(!userExists){
-      return createError(400, "Email et/ou mot de passe incorrect")
-    }
-    console.log(userExists.password)
-    const match = await argon2.verify(userExists.password, password)
-    if(!match){
-      return createError(400, "Email et/ou mot de passe incorrect")
-    }
-  
-    //Je sais que l'email et le mot de passe sont corrects, j'envoi un token au client (permettant d'ainsi l'authentifier)
-    const token = fastify.jwt.sign({ id: userExists._id, role: userExists.role})
-    return {token}
-    // return null
-  })
-  //------- récupérer l'utilisateur par son id
-  fastify.get('/protected', async (request, reply) =>{
-    //Si l'utilisateur ne m'envoie pas de token, je dois lui retourner une erreur
-    // Sinon, je lui retourne un objet contenant la propriété message avec Bienvenue comme valeur
-    const result = await request.jwtVerify()
-  
-    //-----  const result = await request.jwtVerify() remplace ce code :
-    /** 
-    const tokenString = request.headers.authorization // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZTZkMTU4NjdhZGI1M2NkY2JhY2FhYSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjI1NzUzOTQyfQ.bSuxO2z_-ZDXtFZ4bgvqucrkNqDML2e81rxrXXnxzKo
-      const token = tokenString.split(" ")[1]
-      const decoded = jwt.verify(token, "monsupersecretamoiestungangster")
-      if (!decoded) {
-          createError(400, "Mauvais token")
-    }
-    */
-    //----
-    console.log(result)
-    return {message : `Bienvenu`}
-   
-  })
+async function routes(fastify, options) {
 
+fastify.post('/login', async (request, reply) => {
+	//Action qu'on va faire: je récupère l'email et le password dans la requête (request), 
+	// Je cherche si un utilisateur possède cet émail,
+	//S'il existe:
+	// - On vérifie que les password correspondent
+	// - sinon, on génère une erreur
+
+	const {
+		email,
+		password
+	} = request.body
+	const collection = fastify.mongo.db.collection('users')
+
+	const userExists = await collection.findOne({
+		email
+	})
+
+	if (!userExists) {
+		return createError(400, "Email et/ou mot de passe incorrect")
+	}
+	// console.log(userExists.password)
+	const match = await argon2.verify(userExists.password, password)
+
+	if (!match) {
+		return createError(400, "Email et/ou mot de passe incorrect")
+	}
+
+	// je sais que l'émail et le mot de passe sont correctes, j'envoi un token au client
+	//  (permettant d'ainsi l'authentification)
+	// "const token" permet de générer un nouveau token
+	const token = fastify.jwt.sign({
+		id: userExists._id,
+		role: userExists.role
+	})
+
+	return {
+		token
+	}
+})
+
+fastify.get('/protected', async (request, reply) => {
+	// Si l'utilisateur ne m'envoie pas de token, je dois lui retourner une erreur
+	// Sinon, je lui retourne un objet contenant la propriété message avec Bienvenue comme valeur
+	const result = await request.jwtVerify()
+	// const tokenString = request.headers.authorization // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZTZkMTU4NjdhZGI1M2NkY2JhY2FhYSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjI1NzUzOTQyfQ.bSuxO2z_-ZDXtFZ4bgvqucrkNqDML2e81rxrXXnxzKo
+	// 	const token = tokenString.split(" ")[1]
+	// 	const decoded = jwt.verify(token, "monsupersecretamoiestungangster")
+	// 	if (!decoded) {
+	// 		createError(400, "Mauvais token")
+	return {
+		message: "Bienvenue"
+	}
+})
 }
-  
 module.exports = routes
